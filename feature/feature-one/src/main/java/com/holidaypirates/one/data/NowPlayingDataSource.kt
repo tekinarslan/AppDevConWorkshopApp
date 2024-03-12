a6eb33a143abaa7dbec803168932847556f879ad
kemal selim tekinarslan <selim.tekinarslan@gmail.com>
2024-03-12 23:17:47 +0100

    Update NowPlayingDataSource.kt

--- feature/feature-one/src/main/java/com/holidaypirates/one/data/NowPlayingDataSource.kt
+++ feature/feature-one/src/main/java/com/holidaypirates/one/data/NowPlayingDataSource.kt
@@ -1,4 +1,4 @@
-package com.holidaypirates.one.data
+package com.holidaypirates.graphql.data
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
@@ -12,25 +12,25 @@ class NowPlayingDataSource @Inject constructor(private val nowPlayingUseCase: No
     override fun getRefreshKey(state: PagingState<Int, NetworkMovie>): Int? = state.anchorPosition
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, NetworkMovie> {
-        try {
+        return try {
             val nextPage = params.key ?: 1
             val movieResponse = nowPlayingUseCase(nextPage)
 
-            if (movieResponse.body()?.results.isNullOrEmpty()){
-                return LoadResult.Error(throw Exception("Something went wrong"))
+            if (movieResponse.body()?.results.isNullOrEmpty()) {
+                LoadResult.Error(Exception("Something went wrong"))
+            } else {
+                val list = movieResponse.body()?.results ?: emptyList()
+
+                LoadResult.Page(
+                    data = list,
+                    prevKey =
+                    if (nextPage == 1) null
+                    else nextPage - 1,
+                    nextKey = nextPage.plus(1)
+                )
             }
-
-            val list = movieResponse.body()?.results ?: emptyList()
-
-            return LoadResult.Page(
-                data = list,
-                prevKey =
-                if (nextPage == 1) null
-                else nextPage - 1,
-                nextKey = nextPage.plus(1)
-            )
         } catch (t: Throwable) {
-            return LoadResult.Error(t)
+            LoadResult.Error(t)
         }
     }
 }
