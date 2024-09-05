492615072ff8dc273c6823dba03a5c2f20f8e616
K.Selim Tekinarslan <s.tekinarslan@holidaypirates.com>
2024-09-05 09:58:06 +0100

    text changes

--- app/src/main/kotlin/com/holidaypirates/appdevconworkshopapp/MainActivity.kt
+++ app/src/main/kotlin/com/holidaypirates/appdevconworkshopapp/MainActivity.kt
@@ -3,18 +3,23 @@ package com.holidaypirates.appdevconworkshopapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.holidaypirates.designsystem.theme.AppDevconWorkshopAppTheme
@@ -36,7 +41,7 @@ class MainActivity : ComponentActivity() {
                             .padding(8.dp),
                         contentAlignment = Alignment.Center
                     ) {
-                        Greeting("App Dev Con")
+                        Greeting("Droidcon Lisbon 2024 Workshop")
                     }
                 }
             }
@@ -47,11 +52,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Welcome $name",
         modifier = modifier,
+        color = Color.Blue,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center
+        textAlign = TextAlign.Center,
+        lineHeight = 50.sp
     )
 }
 
